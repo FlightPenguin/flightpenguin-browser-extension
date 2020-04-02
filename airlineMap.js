a722cd7ceaa930cfecf147b31059ed8f3693ded6
Sheri Zada <sheri.h.zada@gmail.com>
2020-04-02 15:37:07 -0700

    Add airlineMap file

---
+++ airlineMap.js
@@ -0,0 +1,7 @@
+// We only have Priceline and Southwest so only need to use one mapping system right now.
+// In the future, we can have maps for each provider.
+const airlinesMap = {
+  Southwest: { display: "Southwest", color: "#000" },
+  Delta: { display: "Delta", color: "#000" },
+  American: { display: "American", color: "#000" }
+};
