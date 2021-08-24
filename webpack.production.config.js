e023e59a223ee631bde20459e395816e3e248edd
Max Morlocke <kedlav@gmail.com>
2021-08-24 12:17:24 -0400

    split webpack configs

---
+++ webpack.production.config.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const ts = require("typescript");
+
+const getTsWebpackConfig = (filename = "webpack.config.ts") => {
+  const config = new module.constructor();
+  config.paths = module.paths;
+  config._compile(
+    ts.transpileModule(fs.readFileSync(filename, { encoding: "utf-8" }), {
+      compilerOptions: {
+        module: ts.ModuleKind.CommonJS,
+      },
+    }).outputText,
+    filename,
+  );
+  return config.exports;
+};
+
+const { production } = getTsWebpackConfig();
+module.exports = production;
