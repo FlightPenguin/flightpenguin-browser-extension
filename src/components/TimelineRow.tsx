35d8511c184944a1060c24e558aa96da9ea355ec
Max Morlocke <kedlav@gmail.com>
2021-08-30 15:33:21 -0400

    add layover text for underbelly

--- src/components/TimelineRow.tsx
+++ src/components/TimelineRow.tsx
@@ -129,26 +129,38 @@ export const TimelineRow = ({
           {flightSegments.map((flightSegment) => {
             return (
               <Box
-                key={`flight-segment-${getFlightSegmentId(flightSegment)}`}
-                data-name="flight-segment"
+                display="flex"
                 width={`${flightSegment.layout.width}px`}
                 left={`${flightSegment.layout.startPosition}px`}
-                data-content={flightSegment.from}
-                background={
-                  flightSegment.isLayoverStop
-                    ? "repeating-linear-gradient(45deg, rgba(125, 130, 162, 0.5), rgba(147, 153, 191, 0.5) 5px, rgba(122, 128, 165, 0.5) 5px, rgba(98, 105, 146, 0.5) 10px)"
-                    : undefined
-                }
-                backgroundColor={flightSegment.operatingAirline.color}
+                key={`flight-segment-${getFlightSegmentId(flightSegment)}`}
                 height="30px"
                 position="absolute"
                 tabIndex={-1}
+                flexDirection="column"
               >
-                <Tooltip content={getFlightSegmentName(flightSegment)} hasArrow placement="bottom" tabIndex={-1}>
-                  <Box width={`${flightSegment.layout.width}px`}>
-                    <Text>&nbsp;</Text>
+                <Box
+                  data-name="flight-segment"
+                  data-content={flightSegment.from}
+                  background={
+                    flightSegment.isLayoverStop
+                      ? "repeating-linear-gradient(45deg, rgba(125, 130, 162, 0.5), rgba(147, 153, 191, 0.5) 5px, rgba(122, 128, 165, 0.5) 5px, rgba(98, 105, 146, 0.5) 10px)"
+                      : undefined
+                  }
+                  backgroundColor={flightSegment.operatingAirline.color}
+                >
+                  <Tooltip content={getFlightSegmentName(flightSegment)} hasArrow placement="top" tabIndex={-1}>
+                    <Box width={`${flightSegment.layout.width}px`}>
+                      <Text>&nbsp;</Text>
+                    </Box>
+                  </Tooltip>
+                </Box>
+                {flightSegment.isLayoverStop && (
+                  <Box>
+                    <Text alignX="center" fontSize="150">
+                      {flightSegment.from}
+                    </Text>
                   </Box>
-                </Tooltip>
+                )}
               </Box>
             );
           })}
