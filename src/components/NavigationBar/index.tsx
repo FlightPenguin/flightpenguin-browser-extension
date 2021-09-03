47c4ce0b3221a8e3ef2f9eac3e0926c6657229c3
Max Morlocke <kedlav@gmail.com>
2021-09-03 12:01:59 -0400

    styling fixes

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -15,13 +15,13 @@ export default function NavigationBar() {
     <TopNav border={"none"}>
       <TopNav.Section paddingLeft="major-2">
         <TopNav.Item href="https://flightpenguin.com" fontWeight="semibold">
-          <Image src="/icons/logo.svg" height="44px" alt="FlightPenguin Logo" />
+          <Image src="src/icons/logo.png" height="44px" alt="FlightPenguin Logo" />
         </TopNav.Item>
       </TopNav.Section>
       {profileInfo && profileInfo.email && (
         <TopNav.Section paddingRight="major-2">
           <TopNav.Item>
-            <Button borderRadius="3" style={{ whiteSpace: "nowrap" }} iconAfter="solid-user">
+            <Button borderRadius="3" style={{ whiteSpace: "nowrap" }} iconAfter="solid-user" isStatic>
               {profileInfo.email}
             </Button>
           </TopNav.Item>
