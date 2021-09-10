e0ec21092bd82d06b6c84b35e5a2d0a60cabc5d9
Max Morlocke <kedlav@gmail.com>
2021-09-10 15:19:39 -0400

    remove tabindices from navbar

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -14,14 +14,14 @@ const NavigationBar = () => {
   return (
     <TopNav border={"none"}>
       <TopNav.Section paddingLeft="major-2">
-        <TopNav.Item href="https://flightpenguin.com" fontWeight="semibold">
-          <Image src="src/icons/logo.png" height="44px" alt="FlightPenguin Logo" />
+        <TopNav.Item fontWeight="semibold" tabIndex={-1}>
+          <Image src="src/icons/logo.png" height="44px" alt="FlightPenguin Logo" tabIndex={-1} />
         </TopNav.Item>
       </TopNav.Section>
       {profileInfo && profileInfo.email && (
         <TopNav.Section paddingRight="major-2">
           <TopNav.Item>
-            <Button borderRadius="3" style={{ whiteSpace: "nowrap" }} iconAfter="solid-user" isStatic>
+            <Button borderRadius="3" style={{ whiteSpace: "nowrap" }} iconAfter="solid-user" isStatic tabIndex={-1}>
               {profileInfo.email}
             </Button>
           </TopNav.Item>
