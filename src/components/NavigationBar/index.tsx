46ed2e667147be54d8084d5a58718609bb67cbf0
Max Morlocke <kedlav@gmail.com>
2021-10-01 13:42:08 -0400

    add help link

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Image, TopNav } from "bumbag";
+import { Button, DropdownMenu, Image, Link, TopNav } from "bumbag";
 import React, { useEffect, useState } from "react";
 import UserInfo = chrome.identity.UserInfo;
 
@@ -21,9 +21,37 @@ const NavigationBar = () => {
       {profileInfo && profileInfo.email && (
         <TopNav.Section paddingRight="major-2">
           <TopNav.Item>
-            <Button borderRadius="3" style={{ whiteSpace: "nowrap" }} iconAfter="solid-user" isStatic tabIndex={-1}>
-              {profileInfo.email}
-            </Button>
+            <DropdownMenu
+              menu={
+                <React.Fragment>
+                  <DropdownMenu.Group title="Profile" cursor="default">
+                    <DropdownMenu.Item
+                      style={{ whiteSpace: "nowrap" }}
+                      iconBefore="solid-user"
+                      color="black"
+                      cursor="default"
+                    >
+                      {profileInfo.email}
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Group>
+                  <DropdownMenu.Group title="Actions">
+                    <DropdownMenu.Item
+                      use={Link}
+                      iconBefore="regular-life-ring"
+                      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                      // @ts-ignore
+                      href="mailto:support@flightpenguin.com"
+                    >
+                      Help
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Group>
+                </React.Fragment>
+              }
+            >
+              <Button borderRadius="3" style={{ whiteSpace: "nowrap" }} iconAfter="chevron-down" isStatic tabIndex={-1}>
+                Settings
+              </Button>
+            </DropdownMenu>
           </TopNav.Item>
         </TopNav.Section>
       )}
