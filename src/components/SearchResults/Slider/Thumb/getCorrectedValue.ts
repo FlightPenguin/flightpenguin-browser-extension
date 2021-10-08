ded561a3c85b95227ece2dec909ef8e471b4d594
Max Morlocke <kedlav@gmail.com>
2021-10-08 12:43:59 -0400

    adjust defaults

--- src/components/SearchResults/Slider/Thumb/getCorrectedValue.ts
+++ src/components/SearchResults/Slider/Thumb/getCorrectedValue.ts
@@ -3,14 +3,17 @@ import Decimal from "decimal.js-light";
 import { flightTimeContainerWidth } from "../../../constants";
 import { thumbWidthWrapperValue } from "../constants";
 
-export const getCorrectedValue = (sliderValue: number): number => {
+export const getCorrectedValue = (sliderValue: number, adjustForWidth = true): number => {
   /*
   Zillow's slider attempts to compensate for the width of the slider.
   It does this in a way we'd prefer it not.
   Let's get the uncorrected value, then adjust to suit our preferences - the exact middle of the thumb div.
    */
-  const unadjustedValue = new Decimal(sliderValue)
+  let correctedValue = new Decimal(sliderValue)
     .times(flightTimeContainerWidth - thumbWidthWrapperValue)
     .dividedBy(flightTimeContainerWidth + flightTimeContainerWidth - thumbWidthWrapperValue);
-  return unadjustedValue.plus(new Decimal(thumbWidthWrapperValue).dividedBy(2)).toDecimalPlaces(2).toNumber();
+  if (adjustForWidth) {
+    correctedValue = correctedValue.plus(new Decimal(thumbWidthWrapperValue).dividedBy(2));
+  }
+  return correctedValue.toDecimalPlaces(2).toNumber();
 };
