7d4159e51a487653ea042f94635d5416f9bc2e9e
Max Morlocke <kedlav@gmail.com>
2021-10-27 16:56:23 -0400

    remove date hover

--- src/components/SearchResults/Slider/Thumb/index.tsx
+++ src/components/SearchResults/Slider/Thumb/index.tsx
@@ -80,11 +80,9 @@ const Thumb = ({
       >
         <Icon icon={state.index === 0 ? "solid-plane-departure" : "solid-plane-arrival"} color="white" fontSize="300" />
       </Box>
-      <Tooltip content={`${formattedDate}`} hasArrow placement="right">
-        <Box marginTop="minor-1">
-          <Text fontWeight="700">{formattedTime}</Text>
-        </Box>
-      </Tooltip>
+      <Box marginTop="minor-1" tabIndex={0}>
+        <Text fontWeight="700">{formattedTime}</Text>
+      </Box>
     </Box>
   );
 };
