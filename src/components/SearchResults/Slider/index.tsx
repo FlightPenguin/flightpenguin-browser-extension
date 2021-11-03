c10f18651518d071dcbb4d30e9ab0e06e5eb3c51
Max Morlocke <kedlav@gmail.com>
2021-11-03 13:50:56 -0400

    directly control slider value

--- src/components/SearchResults/Slider/index.tsx
+++ src/components/SearchResults/Slider/index.tsx
@@ -1,6 +1,6 @@
 import { Box } from "bumbag";
 import isEqual from "lodash.isequal";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactSlider from "react-slider";
 
 import { getDateValueInRange } from "../../../shared/utilities/getDateValueInRange";
@@ -31,9 +31,23 @@ const TimelineSlider = ({
   flightCount,
 }: TimelineSliderProps): React.ReactElement => {
   const [touched, setTouched] = useState(false);
+  /*
+    react-slider uses defaultValues only on the initial load.  If the defaultValues change, it is not applied.
+    Our UI shenanigans mask this, but do not solve the problem.  So, we have to directly control state.
+    Note that 112 = 28 * 4, which is a hardcode from our skeleton intervals...
+   */
+  const [ticks, setTicks] = useState(112);
+  const [values, setValues] = useState([0, 112]);
+
+  useEffect(() => {
+    const tickCount = getSliderTicks({ intervals });
+    setTicks(tickCount);
+    if (!touched) {
+      setValues([0, tickCount]);
+    }
+  }, [intervals, touched]);
 
   const { minimumDate, maximumDate } = getAcceptableDateRange({ intervals, startDate });
-  const ticks = getSliderTicks({ intervals });
 
   return (
     <Box
@@ -48,7 +62,7 @@ const TimelineSlider = ({
       <ReactSlider
         ariaLabel={["Depart after", "Arrive before"]}
         ariaValuetext={(state) => `Thumb value ${state.valueNow}`}
-        defaultValue={[0, ticks]}
+        value={values}
         disabled={disabled}
         max={ticks}
         min={0}
@@ -83,6 +97,7 @@ const TimelineSlider = ({
           );
         }}
         onChange={(value) => {
+          setValues(value);
           const { datetime: lowerBoundary } = getDatetimeByTick({ startDate, value: value[0] });
           const { datetime: upperBoundary } = getDatetimeByTick({ startDate, value: value[1] });
           onRangeChange(
