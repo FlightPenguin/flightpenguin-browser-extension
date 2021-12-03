a880deb4e9c46e19b52d91b8eb52eab05e0fc954
Max Morlocke <kedlav@gmail.com>
2021-12-03 15:19:30 -0500

    remove stringify

--- webpack.config.ts
+++ webpack.config.ts
@@ -42,7 +42,7 @@ const basePlugins = [
   new ProgressPlugin({}),
   new DefinePlugin({
     "process.env.BUMBAG_ENV": JSON.stringify("not test"),
-    "process.env.VERSION": JSON.stringify("1.8.2"),
+    "process.env.VERSION": "1.8.2",
   }),
 ];
 
