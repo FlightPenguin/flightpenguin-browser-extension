475bec023662a98c1c880b4fce8796b27f27c254
Max Morlocke <kedlav@gmail.com>
2021-12-16 15:10:13 -0500

    ensure failure does not prevent loading

--- src/components/utilities/geography/getNearbyAirportData.ts
+++ src/components/utilities/geography/getNearbyAirportData.ts
@@ -12,31 +12,36 @@ export const getNearbyAirportData = async ({
   page,
 }: GetNearbyAirportDataProps): Promise<{ options: Airport[] }> => {
   const accessToken = await getAuthToken(false);
-  const response = await fetch(
-    `${API_HOST}/api/airport/location?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&page=${page}`,
-    {
-      method: "GET",
-      credentials: "include",
-      mode: "cors",
-      headers: new Headers({
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-      }),
-    },
-  );
+  try {
+    const response = await fetch(
+      `${API_HOST}/api/airport/location?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&page=${page}`,
+      {
+        method: "GET",
+        credentials: "include",
+        mode: "cors",
+        headers: new Headers({
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        }),
+      },
+    );
 
-  if (response.status === 200) {
-    const rawAirports = await response.json();
-    const airports = rawAirports.map((record: any) => {
-      return {
-        key: record.iataCode,
-        label: record.iataCode,
-        name: record.displayName,
-        location: record.displayLocation,
-        value: record.iataCode,
-      };
-    }) as Airport[];
-    return { options: airports };
+    if (response.status === 200) {
+      const rawAirports = await response.json();
+      const airports = rawAirports.map((record: any) => {
+        return {
+          key: record.iataCode,
+          label: record.iataCode,
+          name: record.displayName,
+          location: record.displayLocation,
+          value: record.iataCode,
+        };
+      }) as Airport[];
+      return { options: airports };
+    }
+    return { options: [] };
+  } catch (e) {
+    console.error(e);
+    return { options: [] };
   }
-  return { options: [] };
 };
