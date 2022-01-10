558fd8d60aa0e19a480040b31a877b8e30871291
Max Morlocke <kedlav@gmail.com>
2022-01-10 17:16:01 -0500

    wire in flight modal properly to kiwi

---
+++ src/kiwi/shared/getKiwiFlightId.ts
@@ -0,0 +1,16 @@
+import { MissingElementLookupError, MissingFieldParserError } from "../../shared/errors";
+
+const INNER_FLIGHT_CARD_WRAPPER_SELECTOR = "div[class*='ResultCardstyled__ResultCardInner']";
+
+export const getKiwiFlightId = (flightCard: HTMLDivElement): string => {
+  const innerContainer = flightCard.querySelector(INNER_FLIGHT_CARD_WRAPPER_SELECTOR) as HTMLDivElement;
+  if (!innerContainer) {
+    throw new MissingElementLookupError("Unable to find inner flight card wrapper");
+  }
+
+  if (!innerContainer.dataset.test) {
+    throw new MissingFieldParserError("Unable to extract kiwi id");
+  }
+
+  return innerContainer.dataset.test;
+};
