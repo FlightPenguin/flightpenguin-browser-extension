bf3de83baede055b1998c0d6eda5a2c293b9135d
Max Morlocke <kedlav@gmail.com>
2022-01-21 12:44:56 -0500

    ensure correct casing

--- src/shared/events/analytics/searchDecision.ts
+++ src/shared/events/analytics/searchDecision.ts
@@ -1,5 +1,5 @@
 import { sendAnalyticsEvent } from "../sendAnalyticsEvent";
 
 export const sendSearchDecision = (providerName: string): void => {
-  sendAnalyticsEvent({ category: "Flight Search", action: "Search Choice Offering", label: providerName });
+  sendAnalyticsEvent({ category: "flight search", action: "Search Choice Offering", label: providerName });
 };
