a4d20760966afb733a8c2a1a776681a3b5cd1298
Max Morlocke <kedlav@gmail.com>
2022-01-26 13:04:37 -0500

    add sentry browser tracing to backend scripts

--- src/trip/contentScript.ts
+++ src/trip/contentScript.ts
@@ -4,6 +4,10 @@ window.Sentry.init({
   dsn: process.env.SENTRY_DSN,
   environment: `${process.env.EXTENSION_ENV}`,
   release: `${process.env.SENTRY_PROJECT}@${process.env.VERSION}`,
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  integrations: [new window.Sentry.Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
 });
 
 import { sendFailedScraper } from "../shared/events";
