b8ec582d5c7ab1003951b2647058dc487466417d
Max Morlocke <kedlav@gmail.com>
2022-01-28 13:00:15 -0500

    add auth token invalidation and retry when appropriate

--- src/auth/getAuthToken.ts
+++ src/auth/getAuthToken.ts
@@ -5,7 +5,23 @@ export const getAuthToken = async (interactive = true): Promise<string> => {
 const getAuthTokenPromise = (interactive: boolean): Promise<string> => {
   return new Promise((resolve) => {
     chrome.identity.getAuthToken({ interactive }, (token) => {
-      resolve(token);
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        window.Sentry.captureMessage(chrome.runtime.lastError);
+        chrome.identity.clearAllCachedAuthTokens(() => {
+          if (interactive) {
+            chrome.identity.getAuthToken({ interactive }, (token) => {
+              resolve(token);
+            });
+          } else {
+            resolve(token);
+          }
+        });
+      } else {
+        resolve(token);
+      }
     });
   });
 };
