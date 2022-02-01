43472742833d726f80790a00a225a31e719383dd
Max Morlocke <kedlav@gmail.com>
2022-02-01 15:44:31 -0500

    increment version for social share

--- webpack.config.ts
+++ webpack.config.ts
@@ -44,7 +44,7 @@ const basePlugins = [
   new ProgressPlugin({}),
   new DefinePlugin({
     "process.env.BUMBAG_ENV": JSON.stringify("not test"),
-    "process.env.VERSION": JSON.stringify("1.11.2"),
+    "process.env.VERSION": JSON.stringify("1.11.3"),
   }),
   new EnvkeyWebpackPlugin({
     permitted: ["SENTRY_DSN", "SENTRY_PROJECT", "GOOGLE_ANALYTICS_TRACKING_ID"],
