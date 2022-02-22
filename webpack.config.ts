089ca9047d3b827bac95a99577050d05551d5a5a
Max Morlocke <kedlav@gmail.com>
2022-02-22 15:45:59 -0500

    rev version

--- webpack.config.ts
+++ webpack.config.ts
@@ -44,7 +44,7 @@ const basePlugins = [
   new ProgressPlugin({}),
   new DefinePlugin({
     "process.env.BUMBAG_ENV": JSON.stringify("not test"),
-    "process.env.VERSION": JSON.stringify("1.14.3"),
+    "process.env.VERSION": JSON.stringify("1.14.4"),
   }),
   new EnvkeyWebpackPlugin({
     permitted: [
