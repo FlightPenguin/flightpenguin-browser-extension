f8694bc4ccdc0e10117e6cd48de0a3ab566e3489
Max Morlocke <kedlav@gmail.com>
2022-03-02 13:13:43 -0500

    handle reselection of flights

--- src/cheapoair/constants.ts
+++ src/cheapoair/constants.ts
@@ -1,1 +1,2 @@
 export const BOOKING_INDICATOR_STORAGE_KEY = "fp-redirect-to-booking";
+export const BOOKING_ID_MAP_STORAGE_KEY = "fp-id-map";
--- src/cheapoair/ui/openBookingLink.ts
+++ src/cheapoair/ui/openBookingLink.ts
@@ -1,8 +1,11 @@
-import { BOOKING_INDICATOR_STORAGE_KEY } from "../constants";
+import { BOOKING_ID_MAP_STORAGE_KEY, BOOKING_INDICATOR_STORAGE_KEY } from "../constants";
 import { getBookingUrl } from "../mappings/getBookingUrl";
 
 export const openBookingLink = async (flightPenguinId: string, idMap: { [keyof: string]: string }): Promise<void> => {
-  const cheapoAirId = idMap[flightPenguinId];
+  // as we open booking links, we 'lose' the state in the content script.
+
+  const persistedMap = getOrSetCachedIdMap(idMap);
+  const cheapoAirId = persistedMap[flightPenguinId];
   if (!cheapoAirId) {
     throw new Error("Unable to find cheapoair id for flight penguin id");
   }
@@ -11,4 +14,13 @@ export const openBookingLink = async (flightPenguinId: string, idMap: { [keyof: 
 
   const url = getBookingUrl(cheapoAirId);
   window.location.href = url;
+};
+
+const getOrSetCachedIdMap = (idMap: { [keyof: string]: string }) => {
+  const cachedMap = sessionStorage.getItem(BOOKING_ID_MAP_STORAGE_KEY);
+  if (!cachedMap) {
+    sessionStorage.setItem(BOOKING_ID_MAP_STORAGE_KEY, JSON.stringify(idMap));
+    return idMap;
+  }
+  return JSON.parse(cachedMap);
 };
