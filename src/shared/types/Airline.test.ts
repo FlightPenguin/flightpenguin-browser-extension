0a6045db4df9a3ef9c93e1c3792f5843814b9360
Max Morlocke <kedlav@gmail.com>
2022-04-19 12:36:49 -0400

    ensure airline only rebuilds when given name only

--- src/shared/types/Airline.test.ts
+++ src/shared/types/Airline.test.ts
@@ -1,3 +1,4 @@
+import { Airline } from "./Airline";
 import { AirlineFactory } from "./factories/Airline";
 
 describe("Airline name lookup successful", () => {
@@ -31,5 +32,32 @@ describe("Airline name lookup fails", () => {
   it("getCode works", () => {
     const airline = AirlineFactory.build({}, { transient: { name: "Testfail Air" } });
     expect(airline.getCode()).toEqual(undefined);
+  });
+});
+
+describe("constructor", () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("calls AirlineMap.getAirlineDetails when missing required field", () => {
+    const getAirlineInfoMock = jest.spyOn(Airline.prototype, "getAirlineInfo");
+
+    new Airline({ name: "MeowAir" });
+    expect(getAirlineInfoMock).toHaveBeenCalledWith("MeowAir");
+  });
+
+  it("does not call AirlineMap.getAirlineDetails when having a generic match", () => {
+    const getAirlineInfoMock = jest.spyOn(Airline.prototype, "getAirlineInfo");
+
+    new Airline({ name: "MeowAir", color: "#000000" });
+    expect(getAirlineInfoMock).toHaveBeenCalledTimes(0);
+  });
+
+  it("does not call AirlineMap.getAirlineDetails when having a full match", () => {
+    const getAirlineInfoMock = jest.spyOn(Airline.prototype, "getAirlineInfo");
+
+    new Airline({ name: "MeowAir", color: "#000000", code: "CAT", alliance: "Feline Alliance" });
+    expect(getAirlineInfoMock).toHaveBeenCalledTimes(0);
   });
 });
