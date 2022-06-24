e3336a953145c9eb56a3a400ee7aeb82e831081e
Max Morlocke <kedlav@gmail.com>
2022-06-24 15:44:23 -0400

    ensure names are cleaned

--- src/shared/types/factories/TripSource.ts
+++ src/shared/types/factories/TripSource.ts
@@ -1,6 +1,7 @@
 import { faker } from "@faker-js/faker";
 import { Factory } from "fishery";
 
+import { BookingSiteMap } from "../../nameMaps/bookingSiteMap";
 import { TripSource, TripSourceInput } from "../TripSource";
 import { getParsedNumber } from "../utilities/getParsedNumber";
 
@@ -9,7 +10,13 @@ const sourceNames = ["momondo", "trip", "kiwi", "southwest", "expedia", "cheapoa
 export const TripSourceInputFactory = Factory.define<TripSourceInput>(({ params }) => {
   const { name = faker.random.arrayElement(sourceNames), id, isFirstParty = false } = params;
   const fare = params.fare ? getParsedNumber(params.fare) : faker.datatype.number({ min: 49, max: 2000 });
-  const displayNames = !!params.displayNames && params.displayNames.length > 0 ? params.displayNames : [name];
+
+  const rawDisplayNames = !!params.displayNames && params.displayNames.length > 0 ? params.displayNames : [name];
+  const map = new BookingSiteMap();
+  const displayNames = rawDisplayNames.map((name) => {
+    const overrideName = map.getMatch(name);
+    return overrideName?.name || name;
+  });
 
   return { displayNames, fare, id, isFirstParty, name } as TripSourceInput;
 });
